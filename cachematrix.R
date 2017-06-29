028aaa20249905cf401808b42da31e174813ea7a
Aron <aronifanger@gmail.com>
2017-06-28 23:14:35 -0300

    Exercise complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache Matrix functions
 
-## Write a short comment describing this function
+## This function is used add new assignments in 
+## a traditional matrix
+## Ex: mtx <- makeCacheMatrix(matrix(c(1,2,3,4),c(2,2)))
+## the mtx matrix has this functions below
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(mean) inv <<- mean
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function solve the mtx matrix
+## Ex: cacheSolve(mtx)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
