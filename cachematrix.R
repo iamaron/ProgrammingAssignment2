669303804b678e18973d2ac713607ec9b0dfd9ec
Aron <aronifanger@gmail.com>
2017-07-02 09:44:35 -0300

    Fixed solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,24 @@
 ## Cache Matrix functions
 
 ## This function is used add new assignments in 
-## a traditional matrix
+## a traditional matrix.
+
+## set(matrix): store the matrix in the structure
+## get(): get the matrix from the structure
+## setinv(inverse): set the inverse matrix in the structure
+## getinv(): get the inverse matrix from the structure
+
 ## Ex: mtx <- makeCacheMatrix(matrix(c(1,2,3,4),c(2,2)))
 ## the mtx matrix has this functions below
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
-  set <- function(y) {
-    x <<- y
+  set <- function(matrix) {
+    x <<- matrix
     inv <<- NULL
   }
   get <- function() x
-  setinv <- function(mean) inv <<- mean
+  setinv <- function(inverse) inv <<- inverse
   getinv <- function() inv
   list(set = set, get = get,
        setinv = setinv,
@@ -20,18 +26,22 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function solve the mtx matrix
+## This function solve the M matrix by getting the
+## inverse matrix from the structure, when the inverse
+## is already solved, or solving the matrix and storing
+## it, when the matrix is not solved yet.
+
 ## Ex: cacheSolve(mtx)
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(M, ...) {
         ## Return a matrix that is the inverse of 'x'
-  inv <- x$getinv()
+  inv <- M$getinv()
   if(!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
-  data <- x$get()
+  data <- M$get()
   inv <- solve(data, ...)
-  x$setinv(inv)
+  M$setinv(inv)
   inv
 }
